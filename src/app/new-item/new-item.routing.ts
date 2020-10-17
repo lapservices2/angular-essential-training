7c291a5bcb7b32b96f45601ffa8c2f9a47df2b31
lapservices2 <72894848+lapservices2@users.noreply.github.com>
2020-10-16 21:19:56 -0400

    using feature ngmodule for route

---
+++ src/app/new-item/new-item.routing.ts
@@ -0,0 +1,9 @@
+import { Routes, RouterModule } from '@angular/router';
+import { MediaItemFormComponent } from './media-item-form.component';
+
+const newItemRoutes: Routes =[
+    {path: 'add', component: MediaItemFormComponent}
+];
+
+export const newItemRouting = RouterModule.forRoot(newItemRoutes);
+
